9e0d72b7e59823f8d9d7e257d3bffb7314e59a06
houndini_geek <sanonloick0@gmail.com>
2024-03-22 18:20:18 -0400

    update

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+
+const CACHE_NAME = 'clicktime-challenge-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  // Add paths to other resources (images, fonts, etc.) used in your game
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
+});
